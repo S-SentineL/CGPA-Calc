f60c8e79ebe39b48ffcb0405a3205c1b3bd6b987
Mahijith <134128533+S-SentineL@users.noreply.github.com>
2024-09-04 02:25:19 +0530

    index.html updated || styles.css initialized
    calc function in script needs to be updated. read the comments provided in script.js. id = result (placeholder)
    function for addCourse needed

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-<script>
-    function Input_data() {
-        var grade =document.getElementById('Box1').value;
-        var cred = parseInt(document.getElementById('Box2').value);
+function Input_data() {
+    var grade =document.getElementById('Box1').value;
+    var cred = parseInt(document.getElementById('Box2').value);
 
-        if (grade =="") {
-            alert("Please fill the grade");
-        return false;
-            }else if(isNaN(cred)|| cred< 0||cred>4){
-                alert("Please enter valid credit");
-                return false;
-            }else {
-            calc(grade, cred);
-        }
-     }
-
-    function calc(grade,cred) {
-        var total_cgpa = 0;
-        var total_credit=0;
-
-        if (grade == "S") {
-            total_cgpa += 10 * cred;
-        } else if (grade == "A") {
-            total_cgpa += 9 * cred;
-        } else if (grade == "B") {
-            total_cgpa += 8 * cred;
-        } else if (grade == "C") {
-            total_cgpa += 7 * cred;
-        } else if (grade == "D") {
-            total_cgpa += 6 * cred;
-        } else if (grade == "E") {
-            total_cgpa += 5 * cred;
-        } else {
-            total_cgpa += 0;
-        }
-
-        total_credit += cred;
-        var cgpa = total_cgpa / total_credit;
-        
-        document.getElementById('Box3').innerHtml = "Your CGPA is:"+cgpa.toFixed(2);
+    if (grade =="") {
+        alert("Please fill the grade");
+    return false;
+        }else if(isNaN(cred)|| cred< 0||cred>4){
+            alert("Please enter valid credit");
+            return false;
+        }else {
+        calc(grade, cred);
+    }
     }
 
+    // function calc() {
+    //     let totalCGPA = 0;
+    //     let totalCredits = 0;
+    
+    //     for (let i = 1; i <= courseCount; i++) {
+    //         const gradeValue = parseFloat(document.getElementById(`grade{i}`).value);
+    //         const creditValue = parseFloat(document.getElementById(`credit{i}`).value);
+    //  }
+    // Try this... have not confirmed yet... use const to get id not var 
 
-</script>
+    // i removed <script> idk kyu
+function calc(grade,cred) {  
+    var total_cgpa = 0;
+    var total_credit=0;
+
+    if (grade == "S") {
+        total_cgpa += 10 * cred;
+    } else if (grade == "A") {
+        total_cgpa += 9 * cred;
+    } else if (grade == "B") {
+        total_cgpa += 8 * cred;
+    } else if (grade == "C") {
+        total_cgpa += 7 * cred;
+    } else if (grade == "D") {
+        total_cgpa += 6 * cred;
+    } else if (grade == "E") {
+        total_cgpa += 5 * cred;
+    } else {
+        total_cgpa += 0;
+    }
+
+    total_credit += cred;
+    var cgpa = total_cgpa / total_credit;
+    
+    document.getElementById('Box3').innerHtml = "Your CGPA is:"+cgpa.toFixed(2);
+}
